5c0583f5464f287eeb9e78155165cde3519cc84a
Chuk <chuk@localhost.localdomain>
2016-09-05 21:35:14 -0500

    forgotten password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'scolas.herokuapp.com' }
 
 end
