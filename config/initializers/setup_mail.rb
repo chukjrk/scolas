03233e9ce3d23b7cecec120a3d579b1b3c4215d5
Chuk <chuk@localhost.localdomain>
2017-01-18 21:00:37 -0600

    fa

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,1 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-	address:				'smtp.sendgrid.net',
-	port:					'587',
-	:authentication			=> :plain,
-	user_name:				'app46489353@heroku.com',
-	password: 				'j7vb8nip9824',
-    domain: 				'scolas.herokuapp.com',
-    enable_starttls_auto: true
-}
+
