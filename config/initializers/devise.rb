e53a747e997dc0669ceedbd731ed8122aac6697a
Chuk <chuk@localhost.localdomain>
2017-01-26 23:56:45 -0600

    check

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'scolas@scolas.com'
+  config.mailer_sender = 'officialscolas@scolas.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
